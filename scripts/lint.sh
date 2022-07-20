9e8584b5565e7e25d0e0740ad5fa947417f9f1f1
Stian Steinbakken <stiansteinbakken94@gmail.com>
2022-07-20 12:25:18 +0200

    feat: return exit code from lint script

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,20 +1,22 @@
-#/bin/bash
+#!/bin/bash
 
 numJavaFiles=(`find . -name "*.java" | wc -l`)
 numKotlinFiles=(`find . -name "*.kt" | wc -l`)
 
-if [ $numJavaFiles != 0 ]; then 
+if [ "$numJavaFiles" != 0 ]; then
    echo "Directory has java files. Formatting accordingly"
-   test -f ./pom.xml && mvn prettier:write 
+   test -f ./pom.xml && mvn prettier:write
    # No gradle + prettier repos yet, so don't know what this command will actually be
    test -f ./gradlew && ./gradlew pretterWrite
+   return $?
 fi
 
-if [ $numKotlinFiles != 0 ]; then 
+if [ "$numKotlinFiles" != 0 ]; then
    echo "Directory has kotlin files. Formatting accordingly"
-   test -f ./pom.xml && mvn antrun:run@ktlint-format
-   test -f ./gradlew && ./gradlew ktlintFormat
+   test -f ./pom.xml && mvn spotless:apply
+   test -f ./gradlew && ./gradlew ktfmtFormat
+   return $?
 fi
 
 test -f ./package.json && npm run prettier:fix
-# test -d frontend && cd frontend && npm run prettier:fix || test -d ../frontend && cd -
+return $?
