c829d7411757a7af809fae189294e68ff94d716b
Stian Steinbakken <stian.steinbakken@posten.no>
2025-01-20 10:39:43 +0100

    feat: add more spaces to letter emoji script

--- scripts/letter_emojies.py
+++ scripts/letter_emojies.py
@@ -1,10 +1,18 @@
 import sys
 
+def convert_char_to_emoji(char):
+    if char.isalnum():
+        return f":alphabet-yellow-{char.lower()}:"
+    elif char == " ":
+        return "   "
+    else:
+        return char
+
 def convert_string_to_emojies(input):
-    return "".join(list(map(lambda x: f":alphabet-yellow-{x.lower()}:" if x.isalnum() else x, input)))
+    return "".join(map(convert_char_to_emoji, input))
 
 def __main__():
-    if(len(sys.argv) != 2):
+    if len(sys.argv) != 2:
         print("Please provide a string to convert")
         return
     print(convert_string_to_emojies(sys.argv[1]))
